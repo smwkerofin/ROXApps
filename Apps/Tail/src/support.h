eabded81771a5d362ea773158a201c7ab7c31e84
Stephen Watson <stephen@kerofin.co.uk>
2001-05-18 09:50:32 +0000

    Needed for save box
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@50 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

---
+++ Apps/Tail/src/support.h
@@ -0,0 +1,18 @@
+/*
+ * support.h - support for re-using gtksavebox
+ *
+ * $Id$
+ */
+
+#ifndef _support_h
+#define _support_h
+
+#ifndef _
+#define _(string) string
+#endif
+
+#endif
+
+/*
+ * $Log$
+ */
