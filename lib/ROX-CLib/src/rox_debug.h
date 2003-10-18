1d36452e18beb0e1f861118be7cc143d60f7e035
Stephen Watson <stephen@kerofin.co.uk>
2003-10-18 11:47:51 +0000

    Default is now to have debug enabled.
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@602 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- lib/ROX-CLib/src/rox_debug.h
+++ lib/ROX-CLib/src/rox_debug.h
@@ -3,16 +3,18 @@
  *
  * Unless DEBUG is define as non-zero all these functions are no-ops
  *
- * $Id$
+ * $Id: rox_debug.h,v 1.1 2001/07/17 14:44:49 stephen Exp $
  */
 
 #ifndef _rox_debug_h
 #define _rox_debug_h
 
-#if defined(DEBUG) && DEBUG
+#if !defined(DEBUG) || DEBUG!=0
 
 /* Should be called at start, if not (or progname==NULL) then progname
    is assumed to be "ROX"
+
+   Now called by rox_init()
 */
 extern void rox_debug_init(const char *progname);
 
@@ -32,5 +34,8 @@ extern void rox_debug_printf(int level, const char *fmt, ...);
 #endif /* !defined(_rox_debug_h) */
 
 /*
- * $Log$
+ * $Log: rox_debug.h,v $
+ * Revision 1.1  2001/07/17 14:44:49  stephen
+ * Added DnD stuff (plus path utils and debug util)
+ *
  */
