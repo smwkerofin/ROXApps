a176451ea5749f2580e9ef084e87ff01a24df642
Stephen Watson <stephen@kerofin.co.uk>
2003-10-22 17:17:01 +0000

    Added rox_init
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@605 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- lib/ROX-CLib/src/rox.c
+++ lib/ROX-CLib/src/rox.c
@@ -1,5 +1,5 @@
 /*
- * $Id: rox.c,v 1.1 2002/02/13 11:00:37 stephen Exp $
+ * $Id: rox.c,v 1.2 2002/04/29 08:17:24 stephen Exp $
  *
  * rox.c - General stuff
  */
@@ -9,7 +9,40 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
+#include <unistd.h>
+
 #include "rox.h"
+#include "options.h"
+
+static gchar *program_name=NULL;
+
+void rox_init(const char *program, int *argc, char ***argv)
+{
+  const gchar *app_dir;
+  gchar *options_file;
+  
+  gtk_init(argc, argv);
+
+  program_name=g_strdup(program);
+  rox_debug_init(program); 
+
+  choices_init();
+
+  app_dir=g_getenv("APP_DIR");
+  if(app_dir) {
+    options_file=g_build_filename(app_dir, "Options.xml", NULL);
+    if(access(options_file, R_OK)==0) {
+      options_init(program);
+    }
+    g_free(options_file);
+  }
+  rox_dnd_init();
+}
+
+const gchar *rox_get_program_name(void)
+{
+  return program_name;
+}
 
 int rox_clib_version_number(void)
 {
@@ -55,6 +88,11 @@ const char *rox_clib_gtk_version_string(void)
 
 /*
  * $Log: rox.c,v $
+ * Revision 1.2  2002/04/29 08:17:24  stephen
+ * Fixed applet menu positioning (didn't work if program was managing more than
+ * one applet window)
+ * Some work for GTK+ 2
+ *
  * Revision 1.1  2002/02/13 11:00:37  stephen
  * Better way of accessing web site (by running a URI file).  Improvement to
  * language checking in rox_resources.c.  Visit by the const fairy in choices.h.
--- lib/ROX-CLib/src/rox.h
+++ lib/ROX-CLib/src/rox.h
@@ -1,5 +1,5 @@
 /*
- * $Id: rox.h,v 1.1 2002/02/13 11:00:37 stephen Exp $
+ * $Id: rox.h,v 1.2 2002/04/29 08:17:25 stephen Exp $
  *
  * rox.h - Top level header for ROX-CLib
  */
@@ -16,6 +16,13 @@
 #include "infowin.h"
 #include "rox_resources.h"
 
+/* Initialize GTK+ and bits of ROX-CLib */
+extern void rox_init(const char *program, int *argc, char ***argv);
+
+/* Return name of program as passed to rox_init.
+   Returns NULL if rox_init not called. */
+extern const gchar *rox_get_program_name(void);
+
 /* Version number of ROX-CLib where x.y.z is encoded as xxyyzz */
 extern int rox_clib_version_number(void);
 extern const char *rox_clib_version_string(void);
@@ -28,6 +35,11 @@ extern const char *rox_clib_gtk_version_string(void);
 
 /*
  * $Log: rox.h,v $
+ * Revision 1.2  2002/04/29 08:17:25  stephen
+ * Fixed applet menu positioning (didn't work if program was managing more than
+ * one applet window)
+ * Some work for GTK+ 2
+ *
  * Revision 1.1  2002/02/13 11:00:37  stephen
  * Better way of accessing web site (by running a URI file).  Improvement to
  * language checking in rox_resources.c.  Visit by the const fairy in choices.h.
