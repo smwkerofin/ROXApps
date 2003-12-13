fccd068cb7440a787dc0f834ab19f6d1278e61fb
Stephen Watson <stephen@kerofin.co.uk>
2003-12-13 19:26:05 +0000

    Exposed functions to escape and unescape uri's. rox_path_get_local() and rox_path_get_path() now unescape uri's.
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@627 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- lib/ROX-CLib/src/rox_dnd.c
+++ lib/ROX-CLib/src/rox_dnd.c
@@ -1,7 +1,7 @@
 /*
  * rox_dnd.c - utilities for using drag & drop with ROX apps.
  *
- * $Id: rox_dnd.c,v 1.6 2003/03/05 15:31:23 stephen Exp $
+ * $Id: rox_dnd.c,v 1.7 2003/08/20 20:57:44 stephen Exp $
  */
 
 #include "rox-clib.h"
@@ -62,8 +62,6 @@ static void drag_data_received(GtkWidget *widget, GdkDragContext *context,
 			       gint x, gint y,
 			       GtkSelectionData *selection_data,
 			       guint info, guint32 time, gpointer user_data);
-static gchar *unescape_uri(const char *uri);
-static gchar *encode_path_as_uri(const guchar *path);
 
 static GdkAtom XdndDirectSave0;
 static GdkAtom xa_text_plain;
@@ -214,16 +212,6 @@ static GSList *uri_list_to_gslist(char *uri_list)
   }
 
   return list;
-}
-
-static const gchar *our_host_name(void)
-{
-  static char host[1025]={0};
-
-  if(!host[0])
-    gethostname(host, sizeof(host));
-
-  return host;
 }
 
 /* User has tried to drop some data on us. Decide what format we would
@@ -434,71 +422,11 @@ static void drag_data_received(GtkWidget      	*widget,
   }
 }
 
-/* handle % escapes in DnD */
-/* Escape path for future use in URI */
-gchar *escape_uri_path(const char *path)
-{
-  const char *safe = "-_./"; /* Plus isalnum() */
-  const guchar *s;
-  gchar *ans;
-  GString *str;
-
-  str = g_string_sized_new(strlen(path));
-
-  for (s = path; *s; s++) {
-    if (!g_ascii_isalnum(*s) && !strchr(safe, *s))
-      g_string_append_printf(str, "%%%02x", *s);
-    else
-      str = g_string_append_c(str, *s);
-  }
-
-  ans = str->str;
-  g_string_free(str, FALSE);
-  
-  return ans;
-}
-
-gchar *encode_path_as_uri(const guchar *path)
-{
-  gchar *tpath = escape_uri_path(path);
-  gchar *uri;
-
-  uri = g_strconcat("file://", our_host_name(), tpath, NULL);
-  g_free(tpath);
-
-  return uri;
-}
-
-gchar *unescape_uri(const char *uri)
-{
-  const gchar *s;
-  gchar *d;
-  gchar *tmp;
-	
-  tmp = g_strdup(uri);
-  for (s = uri, d=tmp; *s; s++, d++) {
-    /*printf("%s\n", s);*/
-    if (*s=='%' && g_ascii_isxdigit(s[1]) &&
-	g_ascii_isxdigit(s[2])) {
-      int c;
-      char buf[3];
-      buf[0]=s[1];
-      buf[1]=s[2];
-      buf[2]=0;
-      c=(int) strtol(buf, NULL, 16);
-      *d=c;
-      s+=2;
-    } else {
-      *d=*s;
-    }
-  }
-  *d=0;
-
-  return tmp;
-}
-
 /*
  * $Log: rox_dnd.c,v $
+ * Revision 1.7  2003/08/20 20:57:44  stephen
+ * Handle % escaped URI's
+ *
  * Revision 1.6  2003/03/05 15:31:23  stephen
  * First pass a conversion to GTK 2
  * Known problems in SOAP code.
--- lib/ROX-CLib/src/rox_path.c
+++ lib/ROX-CLib/src/rox_path.c
@@ -1,7 +1,7 @@
 /*
  * rox_path.c - utilities for path handling, support for drag & drop
  *
- * $Id: rox_path.c,v 1.4 2001/12/05 16:45:02 stephen Exp $
+ * $Id: rox_path.c,v 1.5 2003/03/05 15:31:23 stephen Exp $
  */
 #include "rox-clib.h"
 
@@ -36,9 +36,9 @@ char *rox_path_get_local(const char *uri)
   
   if(uri[0]=='/') {
     if(uri[1]!='/')
-      return g_strdup(uri);
+      return unescape_uri(uri);
     if(uri[2]=='/')
-      return g_strdup(uri+2);
+      return unescape_uri(uri+2);
 
     host=(char *) uri+2;
     end=strchr(host, '/');
@@ -46,7 +46,7 @@ char *rox_path_get_local(const char *uri)
 
     if(strcmp(host, "localhost")==0) {
       g_free(host);
-      return g_strdup(end);
+      return unescape_uri(end);
     }
 
     if(!hostn[0]) {
@@ -60,7 +60,7 @@ char *rox_path_get_local(const char *uri)
 
     if(strcmp(host, hostn)==0) {
       g_free(host);
-      return g_strdup(end);
+      return unescape_uri(end);
     }
 
 #ifdef HAVE_GETHOSTNAME
@@ -69,19 +69,19 @@ char *rox_path_get_local(const char *uri)
       char **alias;
       if(strcmp(hp->h_name, hostn)==0) {
 	g_free(host);
-	return g_strdup(end);
+	return unescape_uri(end);
       }
       for(alias=hp->h_aliases; *alias; alias**)
 	if(strcmp(*alias, hostn)==0) {
 	  g_free(host);
-	  return g_strdup(end);
+	  return unescape_uri(end);
 	}
 
       lhp=gethostbyname(hostn);
       if(lhp) {
 	if(strcmp(hp->h_name, lhp->h_name)==0) {
 	  g_free(host);
-	  return g_strdup(end);
+	  return unescape_uri(end);
 	}
       }
     }
@@ -125,23 +125,100 @@ char *rox_path_get_path(const char *uri)
   
   if(uri[0]=='/') {
     if(uri[1]!='/')
-      return g_strdup(uri);
+      return unescape_uri(uri);
     if(uri[2]=='/')
-      return g_strdup(uri+2);
+      return unescape_uri(uri+2);
 
     host=(char *) uri+2;
     end=strchr(host, '/');
     
-    return g_strdup(end);
+    return unescape_uri(end);
   }
 
   return NULL;
 }
 
 
+/* handle % escapes in DnD */
+/* Escape path for future use in URI */
+gchar *escape_uri_path(const char *path)
+{
+  const char *safe = "-_./"; /* Plus isalnum() */
+  const guchar *s;
+  gchar *ans;
+  GString *str;
+
+  str = g_string_sized_new(strlen(path));
+
+  for (s = path; *s; s++) {
+    if (!g_ascii_isalnum(*s) && !strchr(safe, *s))
+      g_string_append_printf(str, "%%%02x", *s);
+    else
+      str = g_string_append_c(str, *s);
+  }
+
+  ans = str->str;
+  g_string_free(str, FALSE);
+  
+  return ans;
+}
+
+static const gchar *our_host_name(void)
+{
+  static char host[1025]={0};
+
+  if(!host[0])
+    gethostname(host, sizeof(host));
+
+  return host;
+}
+
+gchar *encode_path_as_uri(const guchar *path)
+{
+  gchar *tpath = escape_uri_path(path);
+  gchar *uri;
+
+  uri = g_strconcat("file://", our_host_name(), tpath, NULL);
+  g_free(tpath);
+
+  return uri;
+}
+
+gchar *unescape_uri(const char *uri)
+{
+  const gchar *s;
+  gchar *d;
+  gchar *tmp;
+	
+  tmp = g_strdup(uri);
+  for (s = uri, d=tmp; *s; s++, d++) {
+    /*printf("%s\n", s);*/
+    if (*s=='%' && g_ascii_isxdigit(s[1]) &&
+	g_ascii_isxdigit(s[2])) {
+      int c;
+      char buf[3];
+      buf[0]=s[1];
+      buf[1]=s[2];
+      buf[2]=0;
+      c=(int) strtol(buf, NULL, 16);
+      *d=c;
+      s+=2;
+    } else {
+      *d=*s;
+    }
+  }
+  *d=0;
+
+  return tmp;
+}
+
 
 /*
  * $Log: rox_path.c,v $
+ * Revision 1.5  2003/03/05 15:31:23  stephen
+ * First pass a conversion to GTK 2
+ * Known problems in SOAP code.
+ *
  * Revision 1.4  2001/12/05 16:45:02  stephen
  * More tests to see if a URI is a local path
  *
