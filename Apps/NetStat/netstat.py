6b6eececfa706d69410f609bb2dd93ced71cce50
Stephen Watson <stephen@kerofin.co.uk>
2004-03-17 18:48:39 +0000

    Get the byte count if we can and display that in preference to the packet count. Change to way arrows are draw and labeled
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@666 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- Apps/NetStat/netstat.py
+++ Apps/NetStat/netstat.py
@@ -1,4 +1,4 @@
-# $Id: netstat.py,v 1.5 2003/06/09 17:29:11 stephen Exp $
+# $Id: netstat.py,v 1.6 2003/08/23 17:00:54 stephen Exp $
 
 """Interface to network statistics, under Linux.  The function stat() returns
 the data."""
@@ -8,6 +8,7 @@ import sys
 import string
 import time
 
+#print sys.platform
 if sys.platform[:5]=='linux':
     from linux import stat, sockets
 elif sys.platform=='sunos5':
@@ -20,19 +21,31 @@ else:
         return None
 
 class IfNetStat:
-    def __init__(self, name, rpkt, tpkt):
+    def __init__(self, name, rpkt, tpkt, rbyte=-1, tbyte=-1):
         self.name=name
-        self.prev=(rpkt, tpkt)
-        self.current=(0, 0)
+        self.prev=(rpkt, tpkt, rbyte, tbyte)
+        self.clear()
 
-    def update(self, rpkt, tpkt):
-        self.current=(rpkt-self.prev[0], tpkt-self.prev[1])
-        self.prev=(rpkt, tpkt)
+    def update(self, rpkt, tpkt, rbyte=-1, tbyte=-1):
+        #print 'update'
+        #print self.name, rpkt, tpkt, rbyte, tbyte
+        self.current[0]=rpkt-self.prev[0]
+        self.current[1]=tpkt-self.prev[1]
+        if rbyte>0:
+            self.current[2]=rbyte-self.prev[2]
+        else:
+            self.current[2]=-1
+        if tbyte>0:
+            self.current[3]=tbyte-self.prev[3]
+        else:
+            self.current[3]=-1
+        self.prev=(rpkt, tpkt, rbyte, tbyte)
 
     def clear(self):
-        self.current=None
+        self.current=[0, 0, -1, -1]
 
     def getCurrent(self):
+        #print 'getCurrent', self.prev, self.current
         return self.current
     def getTotal(self):
         return self.prev
@@ -51,15 +64,20 @@ class NetStat:
         ifs=res.keys()
         for iface in ifs:
             try:
-                self.ifs[iface].update(res[iface][0], res[iface][1])
+                self.ifs[iface].update(res[iface][0], res[iface][1],
+                                       res[iface][2], res[iface][3])
             except KeyError:
-                self.ifs[iface]=IfNetStat(iface, res[iface][0], res[iface][1])
+                self.ifs[iface]=IfNetStat(iface, res[iface][0], res[iface][1],
+                                          res[iface][2], res[iface][3])
 
     def getCurrent(self, iface):
+        #print 'getCurrent', self.ifs, iface
         try:
+            #print self.ifs
             stat=self.ifs[iface]
         except KeyError:
             return None
+        #print stat
         return stat.getCurrent()
     
     def getSockets(self, servers=0):
@@ -71,8 +89,8 @@ def test(iface='lo'):
         stats.update()
         res=stats.getCurrent(iface)
         if res:
-            (rpkt, tpkt)=res
-            print '%4s: %8d  %8d' % (iface, rpkt, tpkt)
+            (rpkt, tpkt, rbytes, tbytes)=res
+            print '%4s: %8d %8d  %10d %10d' % (iface, rpkt, tpkt, rbytes, tbytes)
         else:
             print '%4s: not configured' % iface
         time.sleep(2)
