9a5ca494d341ab7e8a5df6d6a289179069ff7ee6
Stephen Watson <stephen@kerofin.co.uk>
2005-08-21 13:06:16 +0000

    Added doxygen comments. Renamed struct to ROXAppInfo
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@1037 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- lib/ROX-CLib/src/appinfo.c
+++ lib/ROX-CLib/src/appinfo.c
@@ -1,7 +1,7 @@
 /*
  * A GObject to hold a parsed AppInfo.xml file
  *
- * $Id$
+ * $Id: appinfo.c,v 1.1 2004/05/22 17:03:57 stephen Exp $
  */
 
 #include "rox-clib.h"
@@ -20,6 +20,11 @@
 #include "rox.h"
 #include "rox_debug.h"
 #include "mime.h"
+
+/**
+ * @file appinfo.c
+ * @brief Parsing an applications AppInfo.xml file.
+ */
 
 static void rox_appinfo_finalize (GObject *object);
 
@@ -49,6 +54,9 @@ static void rox_appinfo_init(GTypeInstance *object, gpointer gclass)
   appinfo->about_no_lang=NULL;
 }
 
+/**
+ * @return GType code for the #ROXAppInfo type.
+ */
 GType rox_appinfo_get_type(void)
 {
 	static GType type = 0;
@@ -75,6 +83,11 @@ GType rox_appinfo_get_type(void)
 	return type;
 }
 
+/**
+ * Construct a new #ROXAppInfo object for the current application.
+ * @return pointer to new #ROXAppInfo object, or @c NULL if the application
+ * does not have an AppInfo.xml file.
+ */
 GObject *rox_appinfo_new(void)
 {
   const gchar *app_dir=rox_get_app_dir();
@@ -93,6 +106,12 @@ GObject *rox_appinfo_new(void)
   return obj;
 }
 
+/**
+ * Construct a new #ROXAppInfo object for specified AppInfo file.
+ * @param[in] path path to AppInfo.xml file
+ * @return pointer to new #ROXAppInfo object, or @c NULL if the application
+ * does not have an AppInfo.xml file.
+ */
 GObject *rox_appinfo_new_from_path(const gchar *path)
 {
   GObject *obj;
@@ -162,9 +181,17 @@ static void scan_appinfo(ROXAppInfo *ai)
   
 }
 
+/**
+ * Set the language used for scanning the AppInfo document.  This updates
+ * #ROXAppInfo.pref_lang, #ROXAppInfo.summary, #ROXAppInfo.about
+ * and #ROXAppInfo.about_no_lang
+ *
+ * @param[in,out] ai parsed AppInfo file
+ * @param[in] lang language code, e.g. en_GB
+ */
 void rox_appinfo_set_language(ROXAppInfo *ai, const gchar *lang)
 {
-  g_return_if_fail(ai!=NULL);
+  g_return_if_fail(ROX_IS_APPINFO(ai));
 
   /*if(!lang)
     lang=g_getenv("LANG");*/
@@ -185,21 +212,31 @@ void rox_appinfo_set_language(ROXAppInfo *ai, const gchar *lang)
   scan_appinfo(ai);
 }
 
+/**
+ * Get the language used for scanning the AppInfo document.
+ * @param[in,out] ai parsed AppInfo file
+ * @return the language code, e.g. en_GB
+ */
 const gchar *rox_appinfo_get_language(ROXAppInfo *ai)
 {
-  g_return_val_if_fail(ai!=NULL, NULL);
+  g_return_val_if_fail(ROX_IS_APPINFO(ai), NULL);
 
   return ai->pref_lang;
 }
 
+/**
+ * Get the contents of the Summary element in the currently set language.
+ * @param[in] ai parsed AppInfo file
+ * @return the summary
+ */
 const gchar *rox_appinfo_get_summary(ROXAppInfo *ai)
 {
-  g_return_val_if_fail(ai!=NULL, NULL);
+  g_return_val_if_fail(ROX_IS_APPINFO(ai), NULL);
 
   return ai->summary;
 }
 
-gchar *get_about_from_node(xmlNodePtr about, const gchar *lbl,
+static gchar *get_about_from_node(xmlNodePtr about, const gchar *lbl,
 			   gboolean want_label)
 {
   xmlNodePtr node;
@@ -228,13 +265,21 @@ gchar *get_about_from_node(xmlNodePtr about, const gchar *lbl,
   return answer;
 }
 
+/**
+ * Get the contents of the named element from the About node in the currently
+ * set language, or the neutral language (#ROXAppInfo.about_no_lang) if that
+ * fails.
+ * @param[in] ai parsed AppInfo file
+ * @param[in] lbl name of element in About to fetch
+ * @return contents of the element, pass to g_free() when done.
+ */
 gchar *rox_appinfo_get_about(ROXAppInfo *ai, const gchar *lbl)
 {
   xmlNodePtr node;
   xmlChar *value;
   gchar *answer=NULL;
 
-  g_return_val_if_fail(ai!=NULL, NULL);
+  g_return_val_if_fail(ROX_IS_APPINFO(ai), NULL);
   g_return_val_if_fail(lbl!=NULL, NULL);
   
   if(ai->about)
@@ -248,13 +293,21 @@ gchar *rox_appinfo_get_about(ROXAppInfo *ai, const gchar *lbl)
   return answer;
 }
 
+/**
+ * Get the label for the named element from the About node in the currently
+ * set language, or the neutral language (#ROXAppInfo.about_no_lang) if that
+ * fails.
+ * @param[in] ai parsed AppInfo file
+ * @param[in] lbl name of element in About to fetch
+ * @return label for the element, pass to g_free() when done.
+ */
 gchar *rox_appinfo_get_about_label(ROXAppInfo *ai, const gchar *lbl)
 {
   xmlNodePtr node;
   xmlChar *value;
   gchar *answer=NULL;
 
-  g_return_val_if_fail(ai!=NULL, NULL);
+  g_return_val_if_fail(ROX_IS_APPINFO(ai), NULL);
   g_return_val_if_fail(lbl!=NULL, NULL);
 
   if(ai->about)
@@ -271,11 +324,17 @@ gchar *rox_appinfo_get_about_label(ROXAppInfo *ai, const gchar *lbl)
   return answer;  
 }
 
+/**
+ * Get the named element from the AppInfo file
+ * @param[in] ai parsed AppInfo file
+ * @param[in] element name of element to fetch
+ * @return the elment
+ */
 const xmlNodePtr rox_appinfo_get_element(ROXAppInfo *ai, const gchar *element)
 {
   xmlNodePtr node;
   
-  g_return_val_if_fail(ai!=NULL, NULL);
+  g_return_val_if_fail(ROX_IS_APPINFO(ai), NULL);
   g_return_val_if_fail(element!=NULL, NULL);
   
   for(node=ai->doc->children->children; node; node=node->next) {
@@ -290,6 +349,13 @@ const xmlNodePtr rox_appinfo_get_element(ROXAppInfo *ai, const gchar *element)
   return NULL;
 }
 
+/**
+ * From the named element from the AppInfo file return a list of MIME types
+ * @param[in] ai parsed AppInfo file
+ * @param[in] element name of element to fetch
+ * @return list of #MIMEType items, pass to
+ * rox_appinfo_free_mime_type_list() when done
+ */
 GList *rox_appinfo_get_mime_type_list(ROXAppInfo *ai, const gchar *element)
 {
   GList *list=NULL;
@@ -317,21 +383,43 @@ GList *rox_appinfo_get_mime_type_list(ROXAppInfo *ai, const gchar *element)
   return list;
 }
 
+/**
+ * Free MIME type list.
+ * @param[in,out] list MIME type list.
+ */
 void rox_appinfo_free_mime_type_list(GList *list)
 {
   g_list_free(list);
 }
 
+/**
+ * Return list of types that the application has declared it can run.
+ * Equivalent to rox_appinfo_get_mime_type_list(<var>ai</var>, "CanRun").
+ * @param[in] ai parsed AppInfo file
+ * @return list of #MIMEType items, pass to
+ * rox_appinfo_free_mime_type_list() when done
+ */
 GList *rox_appinfo_get_can_run_list(ROXAppInfo *ai)
 {
   return rox_appinfo_get_mime_type_list(ai, "CanRun");
 }
 
+/**
+ * Return list of types that the application has declared it can generate
+ * thumbnails images for..
+ * Equivalent to rox_appinfo_get_mime_type_list(<var>ai</var>, "CanThumbnail").
+ * @param[in] ai parsed AppInfo file
+ * @return list of #MIMEType items, pass to
+ * rox_appinfo_free_mime_type_list() when done
+ */
 GList *rox_appinfo_get_can_thumbnail_list(ROXAppInfo *ai)
 {
   return rox_appinfo_get_mime_type_list(ai, "CanThumbnail");
 }
 
 /*
- * $Log$
+ * $Log: appinfo.c,v $
+ * Revision 1.1  2004/05/22 17:03:57  stephen
+ * Added AppInfo parser
+ *
  */
--- lib/ROX-CLib/src/appinfo.h
+++ lib/ROX-CLib/src/appinfo.h
@@ -1,7 +1,7 @@
 /*
  * A GObject to hold a parsed AppInfo.xml file
  *
- * $Id$
+ * $Id: appinfo.h,v 1.1 2004/05/22 17:03:57 stephen Exp $
  */
 
 #ifndef __ROX_APPINFO_H__
@@ -11,28 +11,59 @@
 #include <glib-object.h>
 #include <libxml/parser.h>
 
+/**
+ * @file appinfo.h
+ * @brief Parsing an applications AppInfo.xml file.
+ */
+
+/** Cast pointer into a pointer to ROXAppInfo only if it is valid,
+ * otherwise return @c NULL
+ * @param[in] obj pointer to object
+ * @return pointer to ROXAppInfo or @c NULL for error
+ */
 #define ROX_APPINFO(obj)          G_TYPE_CHECK_INSTANCE_CAST (obj, rox_appinfo_get_type (), ROXAppInfo)
+
+/** Cast pointer into a pointer to ROXAppInfo class only if it is valid,
+ * otherwise return @c NULL
+ * @param[in] klass pointer to object class
+ * @return pointer to ROXAppInfo class or @c NULL for error
+ */
 #define ROX_APPINFO_CLASS(klass)  G_TYPE_CHECK_CLASS_CAST (klass, rox_appinfo_get_type (), ROXAppinfoClass)
+
+/** Check a pointer to ROXAppInfo
+ * @param[in] obj pointer to object
+ * @return non-zero if a pointer to a ROXAppInfo
+ */
 #define ROX_IS_APPINFO(obj)       G_TYPE_CHECK_INSTANCE_TYPE (obj, rox_appinfo_get_type ())
 
-typedef struct _ROXAppInfo       ROXAppInfo;
-typedef struct _ROXAppInfoClass  ROXAppInfoClass;
+typedef struct ROXAppInfo       ROXAppInfo;
+typedef struct ROXAppInfoClass  ROXAppInfoClass;
 
-struct _ROXAppInfo
+/**
+ * @brief Type used to store a parsed AppInfo file
+ */
+struct ROXAppInfo
 {
-  GObject object;
+  GObject object;                /**< We derive from GObject to enable
+				  * reference counting */
 
-  xmlDocPtr doc;
+  xmlDocPtr doc;                 /**< The parsed XML document */
 
-  gchar *pref_lang;
+  gchar *pref_lang;              /**< Prefered language code when
+				  * retrieving the Summary and About
+				  * information */
 
   /* When pref_lang changes, invalidate these */
-  gchar *summary;
-  xmlNodePtr about;
-  xmlNodePtr about_no_lang;
+  gchar *summary;                /**< Summary for current language */
+  xmlNodePtr about;              /**< About node for current language,
+				  * access via rox_appinfo_get_about()
+				  * or rox_appinfo_get_about_label() */
+  xmlNodePtr about_no_lang;      /**< About node for no declared language
+				  * (normally english) */
 };
 
-struct _ROXAppInfoClass
+/** @internal */
+struct ROXAppInfoClass
 {
   GObjectClass parent;
 };
