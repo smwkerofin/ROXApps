cef88fa44ac0eabd8d2a4d9f81e047ba71aed151
Stephen Watson <stephen@kerofin.co.uk>
2005-11-19 12:51:09 +0000

    Trying to fix install of MIME handlers when using the injector.
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@1118 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- Apps/VideoThumbnail/options.py
+++ Apps/VideoThumbnail/options.py
@@ -1,12 +1,14 @@
 """Handle the options for the video thumbnailler"""
 
-# $Id: options.py,v 1.7 2005/04/22 15:24:46 stephen Exp $
+# $Id: options.py,v 1.8 2005/10/08 10:57:22 stephen Exp $
 
 import os
 
 import rox
 import rox.options
 import rox.OptionsBox
+import rox.mime_handler
+import rox.AppInfo
 
 rox.setup_app_options('VideoThumbnail', site='kerofin.demon.co.uk')
 
@@ -23,9 +25,71 @@ scale=rox.options.Option('scale', False)
 rox.app_options.notify()
 
 def install_button_handler(*args):
-    import rox.mime_handler
     try:
-        rox.mime_handler.install_from_appinfo()
+        if rox.roxlib_version>=(2, 0, 3):
+            rox.mime_handler.install_from_appinfo(injint='http://www.kerofin.demon.co.uk/2005/interfaces/VideoThumbnail')
+        else:
+            try:
+                from zeroinstall.injector import basedir
+                have_zeroinstall=True
+            except:
+                have_zeroinstall=False
+
+            in_zeroinstall=False
+            if have_zeroinstall:
+                for d in basedir.xdg_cache_dirs:
+                    if rox._roxlib_dir.find(d)==0 or __file__.find(d)==0:
+                        in_zeroinstall=True
+                        break
+
+            if in_zeroinstall:
+                app_info_path = os.path.join(rox.app_pdir, 'AppInfo.xml')
+                ainfo = rox.AppInfo.AppInfo(app_info_path)
+                can_thumbnail = ainfo.getCanThumbnail()
+
+                win=rox.mime_handler.InstallList(rox.app_dir,
+                                                "thumbnail handler",
+                                'MIME-thumb', can_thumbnail,
+                                """Thumbnail handlers provide support for creating thumbnail images of types of file.  The filer can generate thumbnails for most types of image (JPEG, PNG, etc.) but relies on helper applications for the others.""")
+
+                if win.run()!=rox.g.RESPONSE_ACCEPT:
+                    win.destroy()
+                    return
+
+                try:
+                    types=win.get_active()
+
+                    for tname in types:
+                        mime_type = mime.lookup(tname)
+
+                        sname=rox.mime_handler.save_path('rox.sourceforge.net',
+                                                         'MIME-thumb',
+			      '%s_%s' % (mime_type.media, mime_type.subtype))
+                        
+                        tmp=sname+'.tmp%d' % os.getpid()
+                        f=file(tmp, 'w')
+                        f.write('#!/bin/sh\n\n')
+                        f.write('0launch http://www.kerofin.demon.co.uk/2005/interfaces/VideoThumbnail\n')
+                        f.close()
+                        os.chmod(tmp, 0755)
+                        os.symlink(app_dir, tmp)
+                        os.rename(tmp, path)
+
+                    types=win.get_uninstall()
+
+                    for tname in types:
+                        mime_type = mime.lookup(tname)
+                        
+                        sname=rox.mime_handler.save_path('rox.sourceforge.net',
+                                                         'MIME-thumb',
+                                        '%s_%s' % (mime_type.media, mime_type.subtype))
+                        os.remove(sname)
+                finally:
+                    win.destroy()
+                    
+            else:
+                rox.mime_handler.install_from_appinfo()
+                
     except:
         rox.report_exception()
 
