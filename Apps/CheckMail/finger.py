b4934a96bdc981c3589d0102f5244ab2bc143b43
Stephen Watson <stephen@kerofin.co.uk>
2008-09-05 09:15:10 +0000

    Handle timeouts better
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@1435 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- Apps/CheckMail/finger.py
+++ Apps/CheckMail/finger.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import socket
-
-import gobject
+from socket import gaierror, timeout
+import time
 
 PORT=79
 
@@ -29,19 +29,23 @@ def get_line(sock):
     return ans
 
 class Finger(socket.socket):
-    def __init__(self, target, port=PORT, long_output=False):
+    def __init__(self, target, port=PORT, long_output=False, timeout=None):
         self._port=port
         socket.socket.__init__(self, socket.AF_INET, socket.SOCK_STREAM)
+        self.settimeout(timeout)
         user, host=target.split('@')
         self._user=user
         self._host=host
+        #print 'connect to', host, port, time.time(), long_output, timeout
         self.connect((host, port))
+        #print 'send', user, time.time()
         if long_output:
             self.send('/W %s\r\n' % user)
         else:
             self.send('%s\r\n' % user)
 
         self._ch=None
+        #print 'ready to read', time.time()
 
     def _nextc(self):
         if self._ch is None:
@@ -80,15 +84,15 @@ class Finger(socket.socket):
         return '<Finger %s@%s:%d>' % (self._user, self._host, self._port)
 
 def _test():
-    f=Finger('status@gate')
-    print f
-    while True:
-        l=f.readline()
-        if l is None:
-            break
-        print l[:-1]
+    #f=Finger('status@gate')
+    #print f
+    #while True:
+    #    l=f.readline()
+    #    if l is None:
+    #        break
+    #    print l[:-1]
 
-    f2=Finger('kerofin@post', long_output=True)
+    f2=Finger('kerofin@post', long_output=True, timeout=5)
     for l in f2.readlines():
         print l[:-1]
 
