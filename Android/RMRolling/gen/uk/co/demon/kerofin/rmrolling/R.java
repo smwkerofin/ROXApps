884ddcf5c5ccbf39cf1cd33bbbc3c0fb15750dec
Stephen Watson <stephen@kerofin.co.uk>
2013-03-08 16:41:22 +0000

    Use strings via IDs
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@1482 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- Android/RMRolling/gen/uk/co/demon/kerofin/rmrolling/R.java
+++ Android/RMRolling/gen/uk/co/demon/kerofin/rmrolling/R.java
@@ -39,8 +39,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int db_label=0x7f040005;
         public static final int hello_world=0x7f040001;
+        public static final int hundred=0x7f040009;
         public static final int menu_settings=0x7f040002;
+        public static final int mod_label=0x7f040006;
+        public static final int ob_label=0x7f040004;
+        public static final int result_label=0x7f040007;
+        public static final int roll_label=0x7f040003;
+        public static final int zero=0x7f040008;
     }
     public static final class style {
         /** 
