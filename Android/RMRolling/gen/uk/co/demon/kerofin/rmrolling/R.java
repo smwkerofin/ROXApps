7ed88030f225ccd9cf380aeddb1f3cd96ca627d7
Stephen Watson <stephen@kerofin.co.uk>
2013-03-08 16:35:35 +0000

    Might have focus working
    git-svn-id: file:///home/stephen/src/svn-dir/trunk@1481 34a053ba-7b2c-11dd-91e2-8d6f69d4c713

--- Android/RMRolling/gen/uk/co/demon/kerofin/rmrolling/R.java
+++ Android/RMRolling/gen/uk/co/demon/kerofin/rmrolling/R.java
@@ -14,21 +14,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dbText=0x7f070008;
+        public static final int dbText=0x7f070006;
         public static final int menu_settings=0x7f07000f;
-        public static final int modText=0x7f07000b;
-        public static final int obText=0x7f070005;
+        public static final int modText=0x7f070009;
+        public static final int obText=0x7f070003;
         public static final int resultText=0x7f07000e;
         public static final int rollText=0x7f070002;
         public static final int tableRow1=0x7f070000;
-        public static final int tableRow2=0x7f070003;
-        public static final int tableRow3=0x7f070006;
-        public static final int tableRow4=0x7f070009;
+        public static final int tableRow2=0x7f070004;
+        public static final int tableRow3=0x7f070007;
+        public static final int tableRow4=0x7f07000a;
         public static final int tableRow5=0x7f07000c;
         public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070004;
-        public static final int textView3=0x7f070007;
-        public static final int textView4=0x7f07000a;
+        public static final int textView2=0x7f070005;
+        public static final int textView3=0x7f070008;
+        public static final int textView4=0x7f07000b;
         public static final int textView5=0x7f07000d;
     }
     public static final class layout {
